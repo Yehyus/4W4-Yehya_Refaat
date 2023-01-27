06f5c7186a41bb0162ecaf29194b8fb4f6efbda8
Yehya Refaat <yehyarefaat@hotmail.com>
2023-01-27 11:29:30 -0500

    s1c2 Début du thème

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/** 
+* 
+* Modèle par défaut  
+*/?>
+<?php get_header(); ?>
+<main>
+<?php
+    if(have_posts()): 
+        while (have_posts()): the_post();
+            the_title('<h1>', '</h1>');
+            the_content(); ?>
+            <hr>
+        <?php endwhile;
+    endif;
+?>
+</main>
+<?php get_footer(); ?>
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Laboratoire 1
+## 4W4 - Conception d'interface et développement web
+### Auteur : Yehya Refaat
+#### Description
+- Installation local de Wordpress
+- Création d'un thème simple
+- Création d'un dépôt git local
+- Creation d'un dépôt github
+#### Référence- https://developer.wordpress.org/themes/
