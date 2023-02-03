369fdfa414555e9ee9aa098c831a75f38ff3c26b
Yehya Refaat <yehyarefaat@hotmail.com>
2023-02-03 09:07:10 -0500

    s2c2 header.php ajout de bloginfo()

--- header.php
+++ header.php
@@ -6,8 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
 </head>
+
 <body>
   <header>
-    <h1>4W4 - Création d'interface et développement Web</h1>
-</header>
+    <h3>-<?= bloginfo('name')?></h3>
+    <h3>-<?= bloginfo('description')?></h3>
+    <h3>-</h3>
+  <h1><a href="<?= bloginfo('url')?>"><?= bloginfo('name')?></a></h1>
+  <h2><?= bloginfo('description')?></h2>
+  </header>
   
