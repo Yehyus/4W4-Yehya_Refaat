cadf6275b5fa52223757f1231f8aac116f88d7fc
Yehya Refaat <yehyarefaat@hotmail.com>
2023-03-31 11:26:54 -0400

    Ajout des enseignants et domaine et rajout de css

--- template-parts/search-cours.php
+++ template-parts/search-cours.php
@@ -1,18 +1,21 @@
 <?php
 $titre = get_the_title();
 $sigle = substr($titre, 0, 7);
-$titre_long = substr($titre, 7, -6); 
+$titre_cours = substr($titre, 7, -6); 
 $duree = substr($titre, strpos($titre, '('));
 ?>
 
 <article class="resultat_search">
   <div class="resultat_search_titre">
-    <h3> <a href="<?php the_permalink(); ?>"> <?= $sigle ?></a></h3>
-    <h4> <?= $titre_long; ?> </h4>
+    <h3> <a href="<?php the_permalink(); ?>"> <?= $titre_cours; ?></a></h3>
+    <h3>Sigle: <?= $sigle ?></h3>
     <p> <?= wp_trim_words(get_the_excerpt(), 20) ?> </p>
-    <h6> <?= $duree; ?> </h6>
+    <h6> Durée: <?= $duree; ?> </h6>
     <p>Enseignant: <?php the_field('enseignant'); ?>
     <br>Domaine: <?php the_field('domaine'); ?></br>
   </div>
   <div class="resultat_search_resume"> <?= wp_trim_words(get_the_excerpt(), 50) ?></div>
 </article>
+
+
+
