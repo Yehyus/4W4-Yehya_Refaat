8920a7bb003272ee4a45d9b0070b2110fe9a27e8
Yehya Refaat <yehyarefaat@hotmail.com>
2023-03-31 10:29:44 -0400

    Ajout des articles dans les resultats

--- search.php
+++ search.php
@@ -12,9 +12,9 @@
     if (have_posts()): 
         while (have_posts()) : the_post();
 ?>
-        <div class="search_resultat_titre">
+        <div class="resultat_search_titre">
         <h4 ><a href="<?php the_permalink(); ?>"> <?= get_the_title(); ?></a></h4></div>
-        <div class="search_resultat_extrait"> <?= wp_trim_words(get_the_excerpt(), 50) ?></div>
+        <div class="resultat_search_resume"> <?= wp_trim_words(get_the_excerpt(), 50) ?></div>
         <hr>
     <?php endwhile;
 endif;
