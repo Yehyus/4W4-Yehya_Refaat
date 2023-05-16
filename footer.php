08edf86d8ff452251105ca4340e01ec3fce39cf5
Yehyus <yehyarefaat@hotmail.com>
2023-05-15 21:13:40 -0400

    Réglage du footer

--- footer.php
+++ footer.php
@@ -1,10 +1,32 @@
 <footer class="site__footer">
-    <section><?php the_custom_logo(); ?></section>
-    <section>Yehya Refaat</section>
-    <section>
-        <div class="sidebar">
-            <?php dynamic_sidebar( 'footer_1' ); ?>
+    
+    <section class="colonne1">
+        <div class="texte1">
+            <h1>Contact</h1>
+            <h2>Adresse: 3800 R. Sherbrooke E, Montréal, QC H1X 2A2</h2>
+            <h2>Téléphone: (123) 456-7890</h2>
+            <h2>Courriel: blabla@hotmail.com</h2>
         </div>
+  
+    </section>
+    
+    <section class="colonne2">
+        <div class="texte2">
+            <h1>Liens</h1>
+                <a class="logo" href="https://github.com/Yehyus"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=github&color=ff6b6b" width="32" height="32"></a>
+        </div>
+    </section>
+
+    <section class="colonne3">
+        <div class="texte3">
+            <h1>Réseaux sociaux</h1>
+                <a class="logo" href="https://www.facebook.com/" target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=facebook&color=ff6b6b" width="32" height="32"></a>
+                <a class="logo" href="https://www.instagram.com/"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=instagram&color=ff6b6b" width="32" height="32"></a>
+                <a class="logo" href="https://twitter.com/home"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=twitter&color=ff6b6b" width="32" height="32"></a>
+        </div>
+    </section>
+
+
     </section>
 </footer>
 <?php wp_footer(); ?>
